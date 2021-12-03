2e6449e33712393bf4c3ecc0049b851df870c77d
Jules Debeaumont <deb.jules@outlook.fr>
2021-12-03 14:39:28 +0100

    added file upload constraints

--- src/Form/ApplicationType.php
+++ src/Form/ApplicationType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\File;
 
 class ApplicationType extends AbstractType
@@ -31,10 +32,25 @@ class ApplicationType extends AbstractType
             ])
             ->add('files', FileType::class, [
                 'label' => 'Files',
-                'multiple' => true,
+                'multiple' => true, // avec multiple => true, on à une donc un array de string (fichier)
                 'mapped' => false,
                 'required' => false,
-                'help' => '3 files at most, 2Mb max each'
+                'help' => '3 files at most, 2Mb max each',
+                'constraints' => [
+                    new All([ // constraint All est nécéssaire à cause du multiple => true
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '2M',
+                                'maxSizeMessage' => 'File must be at most 2Mb',
+                                'mimeTypes' => [
+                                    'application/pdf',
+                                    'application/x-pdf',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid PDF',
+                            ])
+                        ]
+                    ])
+                ]
             ]);
     }
 
