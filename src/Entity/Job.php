83084bd30bef0e60de67a89e2201633869d9c311
Jules Debeaumont <deb.jules@outlook.fr>
2021-12-14 20:08:54 +0100

    added slug attribute to Job entity

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -99,6 +99,12 @@ class Job
      */
     private $company;
 
+    /**
+     * @Gedmo\Slug(fields={"name"})
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->applications = new ArrayCollection();
@@ -303,6 +309,18 @@ class Job
     public function setCompany(string $company): self
     {
         $this->company = $company;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
