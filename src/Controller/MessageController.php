4c3314ab626774cb3ebd29eb0c9b40dde125e5a1
Jules Debeaumont <deb.jules@outlook.fr>
2021-12-27 23:25:25 +0100

    message controller

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,18 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\Message;
+use App\Entity\User;
 use App\Form\MessageType;
 use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Console\Exception\InvalidOptionException;
 
 #[Route('/message')]
 class MessageController extends AbstractController
 {
-    #[Route('/', name: 'my_message', methods: ['GET'])]
-    public function index(MessageRepository $messageRepository): Response
+    #[Route('/sent', name: 'messages_sent', methods: ['GET'])]
+    public function sent(Request $request ,MessageRepository $messageRepository, PaginatorInterface $paginator): Response
     {
         $messagesQuery = $messageRepository->createQueryBuilder('m')
             ->where('m.receiver = :currentUser')
@@ -22,19 +25,44 @@ class MessageController extends AbstractController
             ->orderBy('m.createdAt', 'DESC')
             ->getQuery();
 
-        return $this->render('message/index.html.twig', [
-            
+        $messages = $paginator->paginate($messagesQuery, $request->query->getInt('page', 1), 10);
+
+        return $this->render('message/sent.html.twig', [
+            'messages' => $messages
+        ]);
+    }
+
+    #[Route('/received', name: 'message_received', methods: ['GET'])]
+    public function received(Request $request ,MessageRepository $messageRepository, PaginatorInterface $paginator): Response
+    {
+        $messagesQuery = $messageRepository->createQueryBuilder('m')
+            ->where('m.sender = :currentUser')
+            ->setParameter('currentUser', $this->getUser())
+            ->orderBy('m.createdAt', 'DESC')
+            ->getQuery();
+
+        $messages = $paginator->paginate($messagesQuery, $request->query->getInt('page', 1), 10);
+
+        return $this->render('message/received.html.twig', [
+            'messages' => $messages
         ]);
     }
 
     #[Route('/new', name: 'message_new', methods: ['GET','POST'])]
-    public function new(Request $request): Response
+    public function new(Request $request, User $receiver): Response
     {
         $message = new Message();
         $form = $this->createForm(MessageType::class, $message);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $message->setSender($this->getUser());
+            $message->setReceiver($receiver);
+
+            if ($message->getSender() === $message->getReceiver()) {
+                throw new InvalidOptionException("You can't send a message to yourself!");
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($message);
             $entityManager->flush();
@@ -54,35 +82,5 @@ class MessageController extends AbstractController
         return $this->render('message/show.html.twig', [
             'message' => $message,
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'message_edit', methods: ['GET','POST'])]
-    public function edit(Request $request, Message $message): Response
-    {
-        $form = $this->createForm(MessageType::class, $message);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('message_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('message/edit.html.twig', [
-            'message' => $message,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'message_delete', methods: ['POST'])]
-    public function delete(Request $request, Message $message): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$message->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($message);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('message_index', [], Response::HTTP_SEE_OTHER);
     }
 }
